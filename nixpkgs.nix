86a1e828f5225aef35457243b91c5ea75fa977ac
Dmitry Bushev <bushevdv@gmail.com>
2018-02-03 09:54:36 +0300

    Pin nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,5 +1,15 @@
-# Nixpkgs with overlay
+# Nixpkgs with Stackage overlay pinned to `nixpkgs.json` revision
 
-{ nixpkgs ? import <nixpkgs> }:
+{ pkgsPath ? null
+, overlays ? []
+, system ? null }:
 
-nixpkgs { overlays = [ (import ./default.nix) ]; }
+let
+  _pkgs = import <nixpkgs> {};
+  _pkgsPath = if pkgsPath != null
+    then pkgsPath
+    else _pkgs.fetchFromGitHub (_pkgs.lib.importJSON ./nixpkgs.json);
+in
+  import _pkgsPath ({
+    overlays = [ (import ./default.nix) ] ++ overlays;
+  } // (if system != null then { inherit system; } else {}))
