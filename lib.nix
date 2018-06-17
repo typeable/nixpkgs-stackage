db3ad9179cbd89420d95025f3962fd6aeb71f01d
Dmitry Bushev <bushevdv@gmail.com>
2018-06-17 20:28:32 +0300

    Upd callStackage2nix calls impure wrapper

--- lib.nix
+++ lib.nix
@@ -1,22 +1,26 @@
-{ stdenv, lib, cacert, glibcLocales, stackage2nix }:
+{ stdenv, lib, cacert, glibcLocales, stackage2nixWrapper }:
 
 rec {
 
   stackageSrc2nix = { name, src }:
     stdenv.mkDerivation {
-      name = "stackage2nix-${name}";
-      buildInputs = [ stackage2nix ];
-      preferLocalBuild = true;
+      name = "stackage2nixWrapper-${name}";
+
       phases = ["installPhase"];
-      SSL_CERT_FILE="${cacert}/etc/ssl/certs/ca-bundle.crt";
-      LANG = "en_US.UTF-8";
-      LOCALE_ARCHIVE = lib.optionalString stdenv.isLinux "${glibcLocales}/lib/locale/locale-archive";
+      buildInputs = [ stackage2nixWrapper ];
+      preferLocalBuild = true;
+
       installPhase = ''
         export HOME="$TMP"
         mkdir -p "$out"
         cd $out
         stackage2nix "${src}"
       '';
+
+      SSL_CERT_FILE="${cacert}/etc/ssl/certs/ca-bundle.crt";
+      LANG = "en_US.UTF-8";
+      LOCALE_ARCHIVE = lib.optionalString stdenv.isLinux "${glibcLocales}/lib/locale/locale-archive";
+
   };
 
   callStackage2nix = name: src: import (stackageSrc2nix { inherit name src; });
