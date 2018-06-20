a8df2a5ca64b92bfce49dd45efdf27ee1a4709a9
Dmitry Bushev <bushevdv@gmail.com>
2018-06-20 10:35:41 +0300

    Add pure callStackage2nix

--- lib.nix
+++ lib.nix
@@ -1,21 +1,17 @@
-{ stdenv, lib, cacert, glibcLocales, stackage2nixWrapper }:
+{ stdenv, lib, cacert, glibcLocales, stackage2nix, stackage2nixWrapper }:
 
 rec {
 
-  stackageSrc2nix = { name, src }:
+  stackage2nixGeneric = { drv, pname, installPhase, name, src }:
     stdenv.mkDerivation {
-      name = "stackage2nixWrapper-${name}";
+
+      inherit installPhase;
+
+      name = "${pname}-${name}";
 
       phases = ["installPhase"];
-      buildInputs = [ stackage2nixWrapper ];
+      buildInputs = [ drv ];
       preferLocalBuild = true;
-
-      installPhase = ''
-        export HOME="$TMP"
-        mkdir -p "$out"
-        cd $out
-        stackage2nix "${src}"
-      '';
 
       SSL_CERT_FILE="${cacert}/etc/ssl/certs/ca-bundle.crt";
       LANG = "en_US.UTF-8";
@@ -23,6 +19,36 @@ rec {
 
   };
 
-  callStackage2nix = name: src: import (stackageSrc2nix { inherit name src; });
+  stackage2nixWrapperSrc = { name, src }: stackage2nixGeneric {
+    drv = stackage2nixWrapper;
+    pname = "stackage2nixWrapper";
+    installPhase = ''
+      export HOME="$TMP"
+      mkdir -p $out
+      cd $out
+      stackage2nix "${src}"
+    '';
+
+    inherit name src;
+  };
+
+  stackage2nixSrc = { name, src, stackage-lts, all-cabal-hashes, hackage-db }: stackage2nixGeneric {
+    drv = stackage2nix;
+    pname = "stackage2nix";
+    installPhase = ''
+      export HOME="$TMP"
+      mkdir -p $out
+      cd $out
+      stackage2nix --all-cabal-hashes ${all-cabal-hashes} --lts-haskell ${stackage-lts} --hackage-db ${hackage-db}
+    '';
+
+    inherit name src;
+  };
+
+  callStackage2nixWrapper = name: src: import (stackage2nixWrapperSrc { inherit name src; });
+
+  callStackage2nix = name: src: stackage-lts: all-cabal-hashes: hackage-db: import stackage2nixSrc {
+    inherit name src stackage-lts all-cabal-hashes hackage-db;
+  };
 
 }
